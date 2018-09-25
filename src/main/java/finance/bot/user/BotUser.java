3eaac324c56d7cabf16ad0d56ba44d06e1d82af5
msmych <msmych@yandex.com>
2018-09-25 15:59:36 +0200

    BotUser getFullName

--- src/main/java/finance/bot/user/BotUser.java
+++ src/main/java/finance/bot/user/BotUser.java
@@ -2,25 +2,41 @@ package finance.bot.user;
 
 import javax.persistence.*;
 
+import static javax.persistence.EnumType.STRING;
+
 @Entity
 public class BotUser {
 
     @Id
     public int id;
-
-    @Column
-    @Enumerated(EnumType.STRING)
+    @Enumerated(STRING)
     public UserAction userAction;
-
     @Column
     public String firstName;
-
     @Column
     public String lastName;
-
     @Column
     public String username;
-
     @Column
     public String defaultCurrency = "EUR";
+
+    public String getFullName() {
+        StringBuilder sb = new StringBuilder();
+        if (firstName != null) {
+            sb.append(firstName);
+        }
+        if (username != null) {
+            if (firstName != null) {
+                sb.append(" ");
+            }
+            sb.append(username);
+        }
+        if (lastName != null) {
+            if (firstName != null || username != null) {
+                sb.append(" ");
+            }
+            sb.append(lastName);
+        }
+        return sb.toString();
+    }
 }
